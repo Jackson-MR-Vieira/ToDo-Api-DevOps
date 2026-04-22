b9ab053673b58d715369f2379b5497cda8851c30
Jackson Vieira <jackson.vieira@pucpr.edu.br>
2026-04-22 10:32:35 -0300

    creating tests

---
+++ test/test_main.py
@@ -0,0 +1,37 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()["title"] == "Estudar Python em DevOps!"
+
+def test_create_task():
+    response = client.post("/tasks", json={"title": "Estudar Python em DevOps!"})
+    assert response.status_code == 200
+    assert response.json()["title"] == "Estudar Python em DevOps!"
+
+
+def test_get_tasks():
+    response = client.get("/tasks")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_create_multiple_tasks():
+    client.post("/tasks", json={"title": "Task 1"})
+    client.post("/tasks", json={"title": "Task 2"})
+
+    response = client.get("/tasks")
+    assert len(response.json()) >= 2
+
+
+def test_task_structure():
+    response = client.post("/tasks", json={"title": "Nova tarefa"})
+    data = response.json()
+
+    assert "title" in data
+    assert data["title"] == "Nova tarefa"
